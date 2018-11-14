b1ff1d1c49b7ccddf2ce22b3a28e43fb9efb919b
Magh <mac@mac-macbook-air.mynet>
2018-11-14 17:16:43 +0300

    demo

--- app/src/main/java/com/shiffer/StartUpActivity.java
+++ app/src/main/java/com/shiffer/StartUpActivity.java
@@ -9,5 +9,8 @@ public class StartUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_up);
+
+        int a =1;
+
     }
 }
