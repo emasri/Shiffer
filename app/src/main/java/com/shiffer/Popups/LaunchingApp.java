491e64351b701dc1e77747907a22f417725c3c38
Magh <mac@mac-macbook-air.mynet>
2018-11-19 12:50:35 +0300

    added Auth library

---
+++ app/src/main/java/com/shiffer/Popups/LaunchingApp.java
@@ -0,0 +1,14 @@
+package com.shiffer.Popups;
+
+import android.app.Application;
+
+public class LaunchingApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+//        Firebase FireBaseAuth =  new
+
+
+    }
+}
